c2664f31504880abde936d06e9f5fe8b40dd3c25
Stavroula Guerrero <4stacyg@optonline.net>
2018-05-09 16:58:22 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  snack.push("bananas")
+  array.push("bananas")
 end
 
 def add_element_to_start_of_array(array, element)
