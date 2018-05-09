490df92fe403fdca5490ac779b277682fe90fa8c
Stavroula Guerrero <4stacyg@optonline.net>
2018-05-09 16:41:27 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["apples", "nectarines", "pears", "watermelon"]
+  snack = ["apples", "nectarines", "pears", "watermelon"]
 end
 
 def add_element_to_end_of_array(array, element)
-  << "grapes"
+  snack << "grapes"
 end
 
 def add_element_to_start_of_array(array, element)
